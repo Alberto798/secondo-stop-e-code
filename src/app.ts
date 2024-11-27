db60dafcdacf5e1c99747277e8b978aa6e2d3aba
Alberto Peroni <albiperoni98@gmail.com>
2024-11-27 19:58:34 +0100

    caricamento

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,4 @@ import { config } from "dotenv";
 config() 
  
 
-import { insertPost } from "./orm";
-insertPost(
-    "will aunting", 2006, "il genio ribelle", "Migliore"
-)
-
-console.log(insertPost)
-
-import { getAllPosts } from "./orm";
-getAllPosts()
-
-console.log(getAllPosts)
-
-
-import { addPost} from "./orm";
-addPost("deep Impact", 1998, "catastrofico", "Mediocre")
-
-console.log(addPost)
+import *as orm from "./orm";
